55e43fd331c7d17c86f07447df25e1d566ddb23b
SamriddhiL <sala4947@colorado.edu>
2021-08-17 20:57:30 -0600

    testing change

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        hello I'm Sami
       </header>
     </div>
   );
